9f276a3ec42de6ab9d1e40827fb5993ef4b78a77
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2022-09-19 07:52:27 +0300

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigurationService } from './common';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Food inventory management')
+    .setDescription('APIs for food inventory management system')
+    .setVersion('1.0')
+    .addTag('Food inventory management system')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const appConfig: AppConfigurationService = app.get(AppConfigurationService);
   await app.listen(appConfig.port);
 }
